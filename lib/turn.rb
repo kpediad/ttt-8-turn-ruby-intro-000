bc4ebcddfab15b0a202ffe982158a217c2177685
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-03-21 12:15:11 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -14,3 +14,18 @@ def position_taken?(board, index)
   end
 end
 
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def input_to_index(input)
+  index = input.to_i - 1
+end
+
+def move(board, index, char = "X")
+  board[index] = char
+end
