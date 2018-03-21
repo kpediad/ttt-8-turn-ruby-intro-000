b449ecc66198afa4511301125a7c0135b802a766
Konstantinos Pediaditakis <pediadk@gmail.com>
2018-03-21 12:10:53 +0000

    automatically backed up by learn

---
+++ lib/turn.rb
@@ -0,0 +1,16 @@
+def valid_move?(board, index)
+  if index.between?(0, 8) && !position_taken?(board, index)
+    return true
+  else
+    return false
+  end
+end
+
+def position_taken?(board, index)
+  if board[index] == "X" || board[index] == "O"
+    return true
+  elsif board[index] == " " || board[index] == "" || board[index] == nil
+    return false
+  end
+end
+
